b336be5c921e8a5c4ce398a23c027bf808ae0978
V0ix <142316886+V0ix@users.noreply.github.com>
2025-04-16 19:47:13 +0530

    Update kernel.c

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -10,3 +10,22 @@ void kernel_main() {
     while (1); // Halt the CPU 
 }
 
+//Calling the main.c function which will actually enter into OS function calling from there you can go anywhere
+
+extern void print_test_message();
+
+void kernel_main() {
+    const char *message = "Hello from my own OS kernel!";
+    char *video_memory = (char *) 0xb8000;
+
+    for (int i = 0; message[i] != '\0'; i++) {
+        video_memory[i * 2] = message[i];
+        video_memory[i * 2 + 1] = 0x07;
+    }
+
+    print_test_message();
+
+    while (1);
+}
+
+
