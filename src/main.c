a9f6b35d18a5774b8473c1a3b8421194a2ead2c4
V0ix <142316886+V0ix@users.noreply.github.com>
2025-04-16 19:45:41 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,10 @@
-
+// src/main.c
+void print_test_message() {
+    const char *msg = "Yeh now you are entered into My OS.....";
+    char *video = (char *) 0xb8000;
+    for (int i = 0; msg[i] != '\0'; i++) {
+        video[(i+40) * 2] = msg[i];    // start printing from line 1
+        video[(i+40) * 2 + 1] = 0x0A;  // green on black
+    }
+}
+//Above function will be called from the kernel.c file
