c9787a26c768d518d043e1387feb091d92308300
V0ix <142316886+V0ix@users.noreply.github.com>
2025-04-16 19:37:59 +0530

    Update kernel.c

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -1,1 +1,12 @@
+void kernel_main() {
+    const char *message = "Hello from my own OS kernel";
+    char *video_memory = (char *) 0xb8000;
+
+    for (int i = 0; message[i] != '\0'; i++) {
+        video_memory[i * 2] = message[i];      // printing character by character
+        video_memory[i * 2 + 1] = 0x07;         // attribute-byte: light grey on black
+    }
+
+    while (1); // Halt the CPU 
+}
 
